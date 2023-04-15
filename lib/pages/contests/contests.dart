f57668394449aeb40c536095f0aa5dc3edaa9fe1
EsraaMokhtar <89299244+EsraaMokhtar@users.noreply.github.com>
2023-04-15 09:47:32 +0200

    show card with contests information

--- lib/pages/contests/contests.dart
+++ lib/pages/contests/contests.dart
@@ -7,13 +7,13 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:kafilmobile/services/ContestService/ContestService.dart';
 import 'package:kafilmobile/model/ContestModel/ContestModel.dart';
 
+// import 'package:kafilmobile/pages/contests/singleContest';
+
 import 'package:kafilmobile/pages/contests/singleContest';
-
-
-// import 'package:kafilmobile/model/ContestModel/constModel.dart';
+import 'package:kafilmobile/model/ContestModel/constModel.dart';
 
 import 'package:hexcolor/hexcolor.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // final firestore = FirebaseFirestore.instance;
 // import 'package:app1/view/pages/contestpage/singlecontestPage.dart';
@@ -28,26 +28,36 @@ class contestPage extends StatefulWidget {
 class _contestPageState extends State<contestPage> {
 
 
+  // bool isLoad = true;
+  // List<ConstModel> freelancersBody = [];
+  // late Map<String, dynamic> test;
+  // List itemList = [];
+  // final _firestore = FirebaseFirestore.instance;
+  // Future<void> getDataContestData() async {
+  //   final Freelancers = await _firestore.collection('Freelancers').get();
+  //   for (var freelancer in Freelancers.docs) {
+  //     itemList.add(freelancer.data());
+  //   }
+  //   //  print(itemList[1]['name']);
+  // }
+
+  // List DATA = [];
+  // getFreelancersData() async {
+  //   DATA = await FreelancerService().FreelancerData();
+  //   if (DATA != []) {
+  //     isLoad = false;
+  //   }
+  //   setState(() {});
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getFreelancersData();
+  // }
 
    List<Map<String, dynamic>> contestEle = [];
 
-
-  //  List<Contestnetworkmodel> contest = [];
-  
-  //   getdata() async {
-
-
-  //   await firestore.collection('contests').get().then((e) {
-      
-  //     for (var doc in e.docs) {
-  //       // print("${doc.id} => ${doc.data()}");
-  //        contest.add(doc.data() as Contestnetworkmodel);
-        
-  //     }
-  //     print(contest);
-  //   });
-
-  // }
 
   getcontestData() async {
     contestEle = await contestServices().getAllContests();
@@ -81,7 +91,27 @@ class _contestPageState extends State<contestPage> {
               child:ListView.builder(
                   itemBuilder: (BuildContext context, int index) {
                     print(contestEle[index]);
-                    return SingleContest(contest:contestEle[index]);
+                    // return SingleContest(contest:contestEle[index]);
+                    return SingleContest(
+                        Posts: contestEle[index]["Posts"],
+                        Views: contestEle[index]["Views"],
+                        accepted: contestEle[index]["accepted"],
+                        comment: contestEle[index]["comment"],
+                        completed: contestEle[index]["completed"],
+                        conditions: contestEle[index]["conditions"],
+                        contestDuration: contestEle[index]["contestDuration"],
+                        contestants: contestEle[index]["contestants"],
+                        deliveryDuration: contestEle[index]["deliveryDuration"],
+                        description: contestEle[index]["description"],
+                        firstWinner: contestEle[index]["firstWinner"],
+                        sectionId: contestEle[index]["sectionId"],
+                        skills: contestEle[index]["skills"],
+                        title: contestEle[index]["title"],
+                        userId: contestEle[index]["userId"],
+                        userImg: contestEle[index]["userImg"],
+                        userName: contestEle[index]["userName"],
+                        winnersNum: contestEle[index]["winnersNum"],
+                    );
                   },
                     itemCount: contestEle.length-1)
                   )
@@ -91,9 +121,32 @@ class _contestPageState extends State<contestPage> {
 }
 
 class SingleContest extends StatelessWidget {
-  const SingleContest({super.key , required this.contest});
+  // const SingleContest({super.key , required this.contest});
+
+    const SingleContest({super.key , required this.Posts ,required this.Views ,required this.accepted ,required this.comment ,required this.contestDuration, required this.completed ,
+   required this.contestants , required this.deliveryDuration , required this.description , required this.firstWinner ,required this.sectionId , required this.conditions ,
+   required this.skills , required this.title ,required this.userId , required this.userImg , required this.userName , required this.winnersNum});
   // final ConstModel contest ;
-  final contest ;
+  // final contest ;
+    final int Posts;
+    final int Views;
+    final bool accepted;
+    final int comment;
+    final bool completed;
+    final String conditions;
+    final String contestDuration;
+    final int contestants;
+    final String deliveryDuration;
+    final String description;
+    final String firstWinner;
+    final String sectionId;
+    final String skills;
+    final String title;
+    final String userId;
+    final String userImg;
+    final String userName;
+    final String winnersNum;
+    
   @override
   Widget build(BuildContext context) {
     return 
@@ -111,14 +164,15 @@ class SingleContest extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       Text(
-                        "contest.userName",
+                        userName,
                         style: TextStyle(
                             fontSize: 20, fontWeight: FontWeight.bold),
                       ),
                       SizedBox(width: 16),
                       CircleAvatar(
                         backgroundImage: NetworkImage(
-                            "https://images.rawpixel.com/image_png_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQxMDMtbWlzY3RoZW1ldDAwMTQ4YS1pbWFnZS5wbmc.png"),
+                          userImg),
+                            // "https://images.rawpixel.com/image_png_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQxMDMtbWlzY3RoZW1ldDAwMTQ4YS1pbWFnZS5wbmc.png"),
                         radius: 30,
                       
                       ),
@@ -132,25 +186,45 @@ class SingleContest extends StatelessWidget {
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           Text(
-                            'Design',
+                            sectionId,
                             style: TextStyle(fontSize: 14, color: Colors.grey),
                           ),
                           SizedBox(height: 4),
                           // 
-                            InkWell( onTap :(){
-                                Navigator.of(context).push(MaterialPageRoute(builder: (context) => singleContestPage(contest:contest)));
-                            },
-                              child: Text(
-                                  'This design is very important, I like it',
-                                  style: TextStyle(
-                                      fontSize: 14, fontWeight: FontWeight.bold),
-                                ),
-                            ),
-                          // Text(
-                          //   'This design is very important, I like it',
-                          //   style: TextStyle(
-                          //       fontSize: 14, fontWeight: FontWeight.bold),
-                          // ),
+                            // InkWell( onTap :(){
+                            //   Navigator.of(context).push(MaterialPageRoute(builder: (context) => singleContestPage(
+                               
+                            //       // Posts: contestEle[index]["Posts"],
+                            //       // Views: contestEle[index]["Views"],
+                            //       // accepted: contestEle[index]["accepted"],
+                            //       // comment: contestEle[index]["comment"],
+                            //       // completed: contestEle[index]["completed"],
+                            //       // conditions: contestEle[index]["conditions"],
+                            //       // contestDuration: contestEle[index]["contestDuration"],
+                            //       // contestants: contestEle[index]["contestants"],
+                            //       // deliveryDuration: contestEle[index]["deliveryDuration"],
+                            //       // description: contestEle[index]["description"],
+                            //       // firstWinner: contestEle[index]["firstWinner"],
+                            //       // sectionId: contestEle[index]["sectionId"],
+                            //       // skills: contestEle[index]["skills"],
+                            //       // title: contestEle[index]["title"],
+                            //       // userId: contestEle[index]["userId"],
+                            //       // userImg: contestEle[index]["userImg"],
+                            //       // userName: contestEle[index]["userName"],
+                            //       // winnersNum: contestEle[index]["winnersNum"],
+                            //   )));
+                            // },
+                            //   child: Text(
+                            //       title,
+                            //       style: TextStyle(
+                            //           fontSize: 14, fontWeight: FontWeight.bold),
+                            //     ),
+                            // ),
+                          Text(
+                            title,
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
                         ],
                       ),
                       SizedBox(width: 8),
@@ -168,7 +242,7 @@ class SingleContest extends StatelessWidget {
                             
                             Icon(Icons.attach_money , color: Colors.green),
                             SizedBox(width: 2),
-                            Text('150', style: TextStyle(color: Colors.grey)),
+                            Text(firstWinner , style: TextStyle(color: Colors.grey)),
                             SizedBox(width: 8),
                             Icon(Icons.emoji_events, color: Colors.green),
                           ],
@@ -202,21 +276,27 @@ class SingleContest extends StatelessWidget {
                       children: [
                         Row(
                           children: [
-                            Text('12', style: TextStyle(color: Colors.grey)),
+                            Text(
+                              '${comment}',
+                              style: TextStyle(color: Colors.grey)),
                             SizedBox(width: 8),
                             Icon(Icons.chat, color: Colors.grey),
                           ],
                         ),
                         Row(
                           children: [
-                            Text('', style: TextStyle(color: Colors.grey)),
+                            Text(
+                             '${Views}',
+                              style: TextStyle(color: Colors.grey)),
                             SizedBox(width: 8),
                             Icon(Icons.remove_red_eye, color: Colors.grey),
                           ],
                         ),
                         Row(
                           children: [
-                            Text('24', style: TextStyle(color: Colors.grey)),
+                            Text(
+                              '${Posts}',
+                              style: TextStyle(color: Colors.grey)),
                             SizedBox(width: 8),
                             Icon(Icons.cloud_upload, color: Colors.grey),
                           ],
